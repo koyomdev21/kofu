c8b9da7cdb28860158d74f2422de10e26871f311
bangstard <qayyum.ri@aug-tech.com>
2023-02-11 20:50:05 +0800

    added scaffold global key

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:kofu/src/constants/resources/color_manager.dart';
 import 'package:kofu/src/routing/app_router.dart';
+import 'package:kofu/src/utils/scaffold_global_key.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
@@ -12,6 +13,7 @@ class MyApp extends ConsumerWidget {
     final goRouter = ref.watch(goRouterProvider);
     return MaterialApp.router(
       routerConfig: goRouter,
+      scaffoldMessengerKey: snackbarKey,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
